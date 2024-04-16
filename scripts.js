01b6727d0fdff311571da4387ee46c8816061714
jeffcolyn <jeferson.barbosa101@gmail.com>
2024-04-16 13:53:10 -0300

    umidade

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
-let chave = "cebcd482eda57fa9a6714c1c2ba91885"
-
+let chave = "cebcd482eda57fa9a6714c1c2ba91885";
 
 function colocarNaTela(dados){
-    console.log(dados)
-    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
-    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
-    document.querySelector(".descricao").innerHTML = dados.weather[0].description
-    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    console.log(dados);
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name;
+    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C";
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description;
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png";
+
+    // Adiciona a umidade na tela
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%";
 }
 
 async function buscarCidade(cidade){
@@ -17,14 +19,14 @@ async function buscarCidade(cidade){
     "&lang=pt_br" +
     "&units=metric"
     )
-    .then(resposta => resposta.json())
+    .then(resposta => resposta.json());
 
-    colocarNaTela(dados)
+    colocarNaTela(dados);
 }
 
 
 function cliqueiNoBotao(){
-   let cidade = document.querySelector(".input-cidade").value
+   let cidade = document.querySelector(".input-cidade").value;
 
-   buscarCidade(cidade)
+   buscarCidade(cidade);
 }
